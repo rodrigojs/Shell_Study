2658fe765b6e14fc7d0f0eaa405a235bd5fbaea0
takanamir <rtakanami0706@gmail.com>
2020-08-14 17:35:23 +0900

    応用演習(関数、select文、PID、trap)

---
+++ exam/ex7.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+if [ $# -ne 1 ];
+then
+    echo 'argument is wrong'
+    exit 1
+fi
+
+function stop_exam7(){
+    rm exam7.lock
+    exit 0
+}
+
+if [ $1 = 'start' ];
+then
+    if [ -f 'exam7.lock' ];
+    then
+        echo 'Process is already running'
+        exit 0
+    else
+        echo $$ > exam7.lock
+        trap "stop_exam7" 2 15
+        for i in `seq 1 1000`;
+        do
+            echo $i >> output_$$.txt
+            sleep 1
+        done
+        rm exam7.lock
+        exit 0
+    fi
+elif [ $1 = 'stop' ];
+then
+    if [ -f 'exam7.lock' ];
+    then
+        PID=-1
+        while read p;
+        do
+           PID=$p
+        done < 'exam7.lock'
+        kill -15 $PID
+    else
+        echo 'process is not running'
+        exit 0
+    fi
+elif [ $1 = 'status' ];
+then
+    if [ -f 'exam7.lock' ];
+    then
+        PID=-1
+        while read p;
+        do
+            PID=$p
+        done < 'exam7.lock'
+        echo Process is running pid=$PID
+     else
+         echo 'Process is not running'
+     fi
+else
+    echo 'wrong argument'
+    exit 1
+fi
